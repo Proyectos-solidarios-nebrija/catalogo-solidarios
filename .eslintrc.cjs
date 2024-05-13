18c01a310a49a36ee7b81f4b0a7ea62fd1b1fa70
waxer59 <hnboyano@gmail.com>
2024-05-13 09:36:49 +0200

    fix config

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  globals: {
+		NodeJS: true,
+		NodeListOf: true,
+	},
+  env: {
+    browser: true,
+    es2022: true,
+    node: true
+  },
+  extends: ['standard', "plugin:astro/recommended","prettier"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
