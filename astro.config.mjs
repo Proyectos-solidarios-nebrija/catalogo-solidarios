90b0006f32034937aa581f9332ceb58a9fbaf689
waxer59 <hnboyano@gmail.com>
2024-05-14 07:41:52 +0200

    add changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
+import { defaultLang, languages } from './src/i18n/ui'
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [mdx()]
-});
+  integrations: [mdx()],
+  i18n: {
+    defaultLocale: defaultLang,
+    locales: Object.keys(languages),
+    fallback: {
+      en: 'es'
+    }
+  }
+})
