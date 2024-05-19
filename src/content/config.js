d1da3f67ba536505e7ee30f86f9973ab20d42df2
waxer59 <hnboyano@gmail.com>
2024-05-19 18:21:13 +0200

    add fixes

--- src/content/config.js
+++ src/content/config.js
@@ -2,7 +2,8 @@ import { z, defineCollection } from 'astro:content'
 const proyectosCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    year: z.string(),
+    yearStart: z.number(),
+    yearEnd: z.number(),
     webLink: z.optional(z.string()),
     imgPath: z.string()
   })
