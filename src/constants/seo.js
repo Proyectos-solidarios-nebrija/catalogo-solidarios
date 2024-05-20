ac26503037307d9aa9ee3e5bb4a6d436683d750d
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2024-05-20 11:18:55 +0200

    Add seo (#32)

---
+++ src/constants/seo.js
@@ -0,0 +1,1 @@
+export const cannonicalURL = 'https://proyectos-solidarios.nebrijanos.com' // TODO: Modify with the real URL
--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -7,10 +7,8 @@ export const defaultLang = 'es'
 export const ui = {
   es: {
     'header.title': 'Proyectos Solidarios',
-    'header.desc': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae odio ac
-    metus dignissim porta a in lorem. Lorem ipsum dolor sit amet, consectetur
-    adipiscing elit. Nulla eu purus dictum, tristique erat nec, lacinia ex.
-    Pellentesque nec sollicitudin leo.`,
+    'header.desc':
+      '¡Bienvenido a nuestra web! Explora nuestras iniciativas solidarias que hemos creado en el instituto Nebrija, donde fusionamos educación, entretenimiento y solidaridad para beneficiar a la comunidad.',
     'header.scroll': 'Desliza hacia abajo',
     'nav.projects': 'Proyectos',
     'nav.galery': 'Galería',
@@ -20,14 +18,14 @@ export const ui = {
     'alt.nebrijaLogoImg': 'Logo de instituto Nebrija de formación profesional',
     'alt.solidariosLogoImg': 'Logo de proyectos solidarios',
     'alt.projectImg': 'Foto del proyecto',
+    'meta.desc':
+      'Explora las solidarias que hemos creado en el instituto Nebrija, donde fusionamos educación, entretenimiento y solidaridad para beneficiar a la comunidad.',
     'timeline.present': 'Presente'
   },
   en: {
     'header.title': 'Solidarity Projects',
-    'header.desc': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae odio ac
-    metus dignissim porta a in lorem. Lorem ipsum dolor sit amet, consectetur
-    adipiscing elit. Nulla eu purus dictum, tristique erat nec, lacinia ex.
-    Pellentesque nec sollicitudin leo.`,
+    'header.desc':
+      'Welcome to our website! Explore our solidarity initiatives that we have created at Nebrija Institute, where we merge education, entertainment and solidarity to benefit the community.',
     'header.scroll': 'Scroll down',
     'nav.projects': 'Projects',
     'nav.galery': 'Galery',
@@ -37,6 +35,8 @@ export const ui = {
     'alt.nebrijaLogoImg': 'Nebrija vocational training institute logo',
     'alt.solidariosLogoImg': 'Solidarity projects logo',
     'alt.projectImg': 'Project photo',
+    'meta.desc':
+      'Explore the solidarity activities we have created at Nebrija High School, where we merge education, entertainment and solidarity to benefit the community',
     'timeline.presente': 'Present'
   }
 }
