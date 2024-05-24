521c61aa01afaede1044bac3cea78ba6bc080e18
waxer59 <hnboyano@gmail.com>
2024-05-24 08:57:59 +0200

    add fixes

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -9,11 +9,31 @@ export const defaultLang = 'es'
 export const ui = {
   es: {
     'alt.nebrijaLogoImg': 'Logo del instituto Nebrija de formación profesional',
-    'alt.solidariosLogoImg': 'Logo de proyectos solidarios'
+    'alt.solidariosLogoImg': 'Logo de proyectos solidarios',
+    'header.scroll': 'Desliza hacia abajo',
+    'nav.projects': 'Proyectos',
+    'nav.gallery': 'Galería',
+    'nav.whoWeAre': '¿Quiénes somos?',
+    'post.partnerCompanies': 'Empresas Colaboradoras',
+    'timeline.present': 'Presente',
+    '404.notFound': 'No encontrado',
+    '404.desc':
+      'La página que buscas no existe. Por favor, verifica la URL e intenta nuevamente.',
+    '404.back': 'Volver al inicio'
   },
   en: {
     'alt.nebrijaLogoImg': 'Nebrija vocational training institute logo',
-    'alt.solidariosLogoImg': 'Solidarity projects logo'
+    'alt.solidariosLogoImg': 'Solidarity projects logo',
+    'header.scroll': 'Scroll down',
+    'nav.projects': 'Projects',
+    'nav.gallery': 'Gallery',
+    'nav.whoWeAre': 'Who we are?',
+    'post.partnerCompanies': 'Partner Companies',
+    'timeline.present': 'Present',
+    '404.notFound': 'Not found',
+    '404.desc':
+      'The page you are looking for does not exist. Please check the URL and try again.',
+    '404.back': 'Back to home'
   }
 }
 
